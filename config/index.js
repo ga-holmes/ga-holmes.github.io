bab762599df583d8018206f65edd99112bf49b06
ga-holmes <garrett.a.holmes01@gmail.com>
2022-02-25 10:50:11 -0500

    fixed gallery maybe? again

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@
 // See if in development
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'http://localhost:3000'
+export const server = dev ? 'http://localhost:3000' : 'https://ga-holmes-github-io.vercel.app'
