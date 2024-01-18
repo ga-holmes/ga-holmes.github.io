0cb2a419ec111eabed51f2fc65c1c2de9c28d515
ga-holmes <garrett.a.holmes01@gmail.com>
2024-01-18 11:51:19 -0500

    added info button

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 
 const mobileScreen = window.matchMedia("only screen and (max-device-width: 1024px)").matches;
 
+let pos = 0;
 function navScroll(e) {
 
     let scrollPos = window.scrollY;
@@ -9,11 +10,25 @@ function navScroll(e) {
     let nav = document.getElementById("nav")
     let info = document.getElementById("info")
 
+    let topPos = 0;
+    let underPos = 8;
+    let id = null;
+
     if (icons != null && scrollPos / window.screen.height < icons.offsetTop / window.screen.height) {
         nav.style.display = 'none'
 
+        // If using a mobile device, activate animation so icon doesnt get in the way
         if (mobileScreen) {
-            info.style.paddingTop = '0.2%'
+            id = setInterval(frameUp, 2);
+
+            function frameUp() {
+                if (pos <= topPos) {
+                    clearInterval(id);
+                } else {
+                    pos--;
+                    info.style.top = pos + 'vh';
+                }
+            }
         }
 
     }
@@ -21,8 +36,20 @@ function navScroll(e) {
     if (icons === null || scrollPos / window.screen.height >= icons.offsetTop / window.screen.height) {
         nav.style.display = 'flex'
         
+        // If using a mobile device, activate animation so icon doesnt get in the way
         if (mobileScreen) {
-            info.style.paddingTop = '20%'
+
+            id = setInterval(frameDown, 2);
+    
+            function frameDown() {
+                if (pos >= underPos) {
+                    clearInterval(id);
+                } else {
+                    pos++;
+                    info.style.top = pos + 'vh';
+                }
+            }
+
         }
     }
 
